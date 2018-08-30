375559bda2bd5693b8871538d8470c7f9f634617
yook00627 <yook00627@gmail.com>
2018-08-30 13:44:30 -0700

    counting the output

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,13 +3,15 @@
 from requests import get
 
 
-def recurse(subreddit, hot_list=[], after=""):
+def count_words(subreddit, word_list, after="", counter={}, t=0):
     """prints titles of first 10 hot posts listed for a given subreddit
     Args:
         subreddit: input name of the subreddit
-        hot_list: the list of titles of the reddit post
-        after: the key for accesing the next part of the api
+        word_list: input list of words to look for
     """
+    if t == 0:
+        for word in word_list:
+            counter[word] = 0
     headers = {'User-Agent': 'yook00627'}
     json = get('https://api.reddit.com/r/{}/hot?after={}'.
                format(subreddit, after), headers=headers).json()
@@ -17,9 +19,13 @@ def recurse(subreddit, hot_list=[], after=""):
         key = json['data']['after']
         parent = json['data']['children']
         for obj in parent:
-            hot_list.append(obj['data']['title'])
+            for word in counter:
+                counter[word] += obj['data']['title'].lower().split(' ').count(word)
         if key is not None:
-            recurse(subreddit, hot_list, key)
-        return hot_list
+            count_words(subreddit, word_list, key, counter, 1)
+        else:
+            for word in sorted(word_list):
+                if counter[word] != 0:
+                    print('{}: {}'.format(word, counter[word]))
     except Exception:
         return None
